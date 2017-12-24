23611505e9eaccbf917314292db5edb2f493d108
Kevin Delemme <kevin.delemme@transferwise.com>
2017-12-24 12:26:43 +0100

    Share store with Expenses

--- src/expenses/index.js
+++ src/expenses/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+
 class Expenses extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <h1>Expenses</h1>;
+    return (
+      <div>
+        <h1>Expenses</h1>
+      </div>
+    );
   }
 }
 
-export default Expenses;
+export default connect(state => state)(Expenses);
