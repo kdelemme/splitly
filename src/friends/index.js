b141f6691ec850debd54685f0c94eecb7bc641e2
Kevin Delemme <kevin.delemme@transferwise.com>
2017-12-24 12:32:40 +0100

    Simplify Friend component dispatcher

--- src/friends/index.js
+++ src/friends/index.js
@@ -7,28 +7,16 @@ import FriendList from "./friend-list";
 import FriendForm from "./friend-form";
 
 class Friends extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-  }
-
-  onSubmit(friend) {
-    this.props.dispatch(addFriend(friend));
-  }
-
-  onDelete(friend) {
-    this.props.dispatch(deleteFriend(friend));
-  }
-
   render() {
-    const { friends } = this.props;
+    const { friends, dispatch } = this.props;
+    const dispatchAddFriend = friend => dispatch(addFriend(friend));
+    const dispatchDeleteFriend = friend => dispatch(deleteFriend(friend));
+
     return (
       <div>
         <h1>Friends</h1>
-        <FriendForm onSubmit={this.onSubmit} />
-        <FriendList friends={friends} onDelete={this.onDelete} />
+        <FriendForm onSubmit={dispatchAddFriend} />
+        <FriendList friends={friends} onDelete={dispatchDeleteFriend} />
       </div>
     );
   }
