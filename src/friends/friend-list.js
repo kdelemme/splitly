bd710c0ccc30034270c582eb614dc9fcf72e5993
Kevin Delemme <kevin.delemme@transferwise.com>
2017-12-24 11:48:59 +0100

    Simplify friend list component

--- src/friends/friend-list.js
+++ src/friends/friend-list.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class FriendList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   renderFriend(friend) {
     return (
       <li key={friend.id}>
