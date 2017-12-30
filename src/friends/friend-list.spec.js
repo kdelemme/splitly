5b1e080747bd96737a414dee2351ae88cb91aa3a
Kevin Delemme <kevin.delemme@transferwise.com>
2017-12-30 16:52:58 +0100

    Refactor test for friend form component

--- src/friends/friend-list.spec.js
+++ src/friends/friend-list.spec.js
@@ -2,13 +2,15 @@ import React from "react";
 import { shallow } from "enzyme";
 import FriendList from "./friend-list";
 
-test("should render two <li> elements", () => {
-  const friends = [{ id: 1, name: "John" }, { id: 2, name: "Alice" }];
-  const wrapper = shallow(<FriendList friends={friends} />);
-  expect(wrapper.find("li").length).toBe(2);
-});
+describe("<FriendList />", () => {
+  test("should render two <li> elements", () => {
+    const friends = [{ id: 1, name: "John" }, { id: 2, name: "Alice" }];
+    const wrapper = shallow(<FriendList friends={friends} />);
+    expect(wrapper.find("li").length).toBe(2);
+  });
 
-test("should render no element if friends is empty", () => {
-  const wrapper = shallow(<FriendList friends={[]} />);
-  expect(wrapper.find("li").length).toBe(0);
+  test("should render no element if friends is empty", () => {
+    const wrapper = shallow(<FriendList friends={[]} />);
+    expect(wrapper.find("li").length).toBe(0);
+  });
 });
