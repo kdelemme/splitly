64e6bc87ab67c7224d4a289b3f7fd9424e0c9b7c
Kevin Delemme <kevin.delemme@transferwise.com>
2018-01-20 09:18:42 +0000

    Add expense preloaded state

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,6 +6,18 @@ const preloadedState = {
     { id: "281847ea-fbe3-49d0-be47-f42a4ddfa433", name: "Alice" },
     { id: "52cd275d-c3be-4cab-b0e0-564f52604e52", name: "Bob" },
   ],
+  expenses: [
+    {
+      id: "835a140b-7eb0-4285-b9ba-97168f5e886c",
+      amount: 44.00,
+      reason: "Cleaning",
+      paidBy: { id: "281847ea-fbe3-49d0-be47-f42a4ddfa433", name: "Alice" },
+      participants: [
+        { id: "281847ea-fbe3-49d0-be47-f42a4ddfa433", name: "Alice" },
+        { id: "52cd275d-c3be-4cab-b0e0-564f52604e52", name: "Bob" },
+      ]
+    }
+  ]
 };
 
 export const store = createStore(reducers, preloadedState);
