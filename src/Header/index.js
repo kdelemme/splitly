5202fee8cadfc91748f10444942cc84f8c5bfb1c
Kevin Delemme <kevin.delemme@transferwise.com>
2018-02-03 14:51:31 +0000

    Add react router

---
+++ src/Header/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark mb-5">
+      <a className="navbar-brand" href="#">
+        Splitly
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navigation"
+        aria-controls="navigation"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navigation">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/expenses">
+              Expenses
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/friends">
+              Friends
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
