957dd1705daaab787ef91cac682353c50fbc381c
Kevin Delemme <kevin.delemme@transferwise.com>
2018-02-03 14:58:41 +0000

    Add circle continuous integration config

--- src/ExpenseForm/index.spec.js
+++ src/ExpenseForm/index.spec.js
@@ -3,25 +3,26 @@ import { shallow } from "enzyme";
 import { ExpenseForm } from ".";
 
 describe("<ExpenseForm />", () => {
+  const FRIENDS = [{ id: 1, name: "Jane" }];
   test("should initialize the state with default values", () => {
     const wrapper = shallow(<ExpenseForm friends={[]} onSubmit={() => ({})} />);
     expect(wrapper.state()).toEqual({ reason: "", amount: "", payer: null, participants: [] });
   });
 
   it("should render with disabled button", () => {
-    const wrapper = shallow(<ExpenseForm friends={[]} />);
+    const wrapper = shallow(<ExpenseForm friends={FRIENDS} />);
     expect(wrapper.find("button[disabled=true]").exists()).toBe(true);
   });
 
   it("should enable submit button if form is valid", () => {
-    const wrapper = shallow(<ExpenseForm />);
+    const wrapper = shallow(<ExpenseForm friends={FRIENDS} />);
     wrapper.setState({ amount: 123, reason: "example", payer: 1, participants: [1, 2, 3] });
 
     expect(wrapper.find("button[disabled=false]").exists()).toBe(true);
   });
 
   it("should disable submit button if amount is not a valid number", () => {
-    const wrapper = shallow(<ExpenseForm />);
+    const wrapper = shallow(<ExpenseForm friends={FRIENDS} />);
 
     wrapper.setState({ amount: "12,2", reason: "example", payer: 1, participants: [1, 2, 3] });
     expect(wrapper.find("button[disabled=true]").exists()).toBe(true);
