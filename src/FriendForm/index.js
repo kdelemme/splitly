0acca00ed98a4b0cdf25f1e3d3be05bac2b1b69d
Kevin Delemme <kevin.delemme@transferwise.com>
2018-02-03 14:16:57 +0000

    Trim Friends name on submit

--- src/FriendForm/index.js
+++ src/FriendForm/index.js
@@ -15,19 +15,20 @@ export class FriendForm extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.dispatchAddFriend(this.state.name);
+    this.props.dispatchAddFriend(this.state.name.trim());
     this.setState({ name: "" });
   };
 
   render() {
     const { name } = this.state;
+    const isValid = name && name.trim().length > 0;
 
     return (
       <div className="row">
         <h1 className="col-12">Add friend</h1>
         <form className="form-inline col-12" onSubmit={this.handleSubmit}>
           <input className="form-control" type="text" value={name} onChange={this.handleChange} placeholder="Name" />
-          <button type="submit" className="btn btn-primary ml-2">
+          <button type="submit" className="btn btn-primary ml-2" disabled={!isValid}>
             Add
           </button>
         </form>
--- src/FriendForm/index.spec.js
+++ src/FriendForm/index.spec.js
@@ -7,18 +7,18 @@ describe("<FriendForm />", () => {
   const EMPTY_EVENT = { preventDefault: EMPTY_FUNCTION };
   const CHANGE_EVENT = value => ({ target: { value } });
 
-  test("should initiate an empty name", () => {
+  it("should initiate an empty name", () => {
     const wrapper = shallow(<FriendForm />);
     expect(wrapper.state("name")).toBe("");
   });
 
-  test("should reflect name change on the state", () => {
+  it("should reflect name change on the state", () => {
     const wrapper = shallow(<FriendForm />);
     wrapper.find('input[type="text"]').simulate("change", CHANGE_EVENT("a new name"));
     expect(wrapper.state("name")).toBe("a new name");
   });
 
-  test("should reset the name state on submit", () => {
+  it("should reset the name state on submit", () => {
     const dispatchSpy = jest.fn();
     const wrapper = shallow(<FriendForm dispatchAddFriend={dispatchSpy} />);
     wrapper.find('input[type="text"]').simulate("change", CHANGE_EVENT("John"));
@@ -26,12 +26,33 @@ describe("<FriendForm />", () => {
     expect(wrapper.state("name")).toBe("");
   });
 
-  test("should pass the name in the onSubmit callback", () => {
+  it("should pass the name in the onSubmit callback", () => {
     const dispatchSpy = jest.fn();
     const wrapper = shallow(<FriendForm dispatchAddFriend={dispatchSpy} />);
     wrapper.find('input[type="text"]').simulate("change", CHANGE_EVENT("John"));
     wrapper.find("form").simulate("submit", EMPTY_EVENT);
 
     expect(dispatchSpy).toHaveBeenCalledWith("John");
+  });
+
+  it("should trim the name in the onSubmit callback", () => {
+    const dispatchSpy = jest.fn();
+    const wrapper = shallow(<FriendForm dispatchAddFriend={dispatchSpy} />);
+    wrapper.find('input[type="text"]').simulate("change", CHANGE_EVENT("   John   "));
+    wrapper.find("form").simulate("submit", EMPTY_EVENT);
+
+    expect(dispatchSpy).toHaveBeenCalledWith("John");
+  });
+
+  it("should disable the submit button if invalid form", () => {
+    const wrapper = shallow(<FriendForm />);
+
+    expect(wrapper.find("button[disabled=true]").exists()).toBe(true);
+  });
+
+  it("should enable the submit button if valid form", () => {
+    const wrapper = shallow(<FriendForm />);
+    wrapper.find('input[type="text"]').simulate("change", CHANGE_EVENT("John"));
+    expect(wrapper.find("button[disabled=false]").exists()).toBe(true);
   });
 });
