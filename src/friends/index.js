d0067967bbe32a5fa82a57eed1a89e382622c381
Kevin Delemme <kevin.delemme@transferwise.com>
2018-02-04 11:24:42 +0000

    Display message when empty friend list

--- src/friends/index.js
+++ src/friends/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Friend } from "./friend";
 import { deleteFriend } from "./actions";
@@ -8,15 +8,28 @@ const mapDispatchToProps = dispatch => ({
   dispatchDeleteFriend: id => () => dispatch(deleteFriend(id))
 });
 
-export const Friends = ({ friends, dispatchDeleteFriend }) => {
-  return (
-    <div className="row">
-      <h1 className="col-12">Friends</h1>
+export class Friends extends Component {
+  renderFriends = () => {
+    const { friends, dispatchDeleteFriend } = this.props;
+    if (!friends || friends.length === 0) {
+      return <p className="col-12 lead">Add some friends...</p>;
+    }
+
+    return (
       <ul className="col-12">
         {friends.map(friend => <Friend key={friend.id} {...friend} onDelete={dispatchDeleteFriend} />)}
       </ul>
-    </div>
-  );
-};
+    );
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <h1 className="col-12">Friends</h1>
+        {this.renderFriends()}
+      </div>
+    );
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Friends);
--- src/friends/index.spec.js
+++ src/friends/index.spec.js
@@ -3,14 +3,15 @@ import { shallow } from "enzyme";
 import { Friends } from ".";
 
 describe("<Friends />", () => {
-  test("should render two <li> elements", () => {
+  it("should render two <li> elements", () => {
     const friends = [{ id: 1, name: "John" }, { id: 2, name: "Alice" }];
     const wrapper = shallow(<Friends friends={friends} />);
     expect(wrapper.find("Friend").length).toBe(2);
   });
 
-  test("should render no element if friends is empty", () => {
+  it("should render no element if friends is empty", () => {
     const wrapper = shallow(<Friends friends={[]} />);
     expect(wrapper.find("li").length).toBe(0);
+    expect(wrapper.find("p").length).toBe(1);
   });
 });
